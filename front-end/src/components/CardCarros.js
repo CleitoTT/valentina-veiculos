9b1adef35f59ea926f794f5ecce7e6035b3940ee
Leonardo <leo.msouza1706@gmail.com>
2024-09-09 16:03:51 -0300

    Adição novas imagens carros e alteração tamanho imagens CardCarros

--- front-end/src/components/CardCarros.js
+++ front-end/src/components/CardCarros.js
@@ -6,7 +6,7 @@ export default function CardCarros({ nome, valor, ano, km, id, imagem }) {
 
     return (
         <div className="w-7/12 lg:w-6/12 h-96 bg-[#FFF] rounded-xl flex flex-col items-center justify-center mt-8">
-            <img src={imageUrl} alt="imagem carro" className="w-8/12 lg:w-10/12" />
+            <img src={imageUrl} alt="imagem carro" className="w-8/12 lg:w-8/12" />
             <div className="w-9/12 flex flex-col">
                 <h1 className="text-2xl font-bold">{nome}</h1>
                 <h1 className="text-2xl font-bold text-vermelho">{valor}</h1>
