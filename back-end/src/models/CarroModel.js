7d8564be5725e52396f8649ca572e951a48a2b4c
Leonardo Machado <leo.msouza1706@gmail.com>
2024-10-11 11:33:27 -0300

    Atualização model especificando a coleção

--- back-end/src/models/CarroModel.js
+++ back-end/src/models/CarroModel.js
@@ -1,15 +1,16 @@
 import mongoose from "mongoose";
 
 const carroSchema = new mongoose.Schema({
-    id: {type: mongoose.Schema.Types.ObjectId},
-    nome: { type: String, required: true},
+    id: { type: mongoose.Schema.Types.ObjectId },
+    nome: { type: String, required: true },
     ano: { type: Number },
     valor: { type: Number },
     km: { type: Number },
     imagem: { type: String },
     marca: { type: String }
-})
+}, { collection: 'carros' }); // Especifica a coleção como 'carros'
 
-const carro = mongoose.model('carros', carroSchema)
+const carro = mongoose.model('Carro', carroSchema);
 
-export default carro
+export default carro;
+
